195f5fedc9b81f936b0fe34460c1d190917c9f2a
Developer <developer@it.step>
2023-02-27 21:02:44 +0300

    aiohttp

---
+++ aiohttp/collector/logger.py
@@ -0,0 +1,9 @@
+import logging
+
+
+logging.basicConfig(filename='collector.log',
+                    format='%(asctime)s %(message)s',
+                    filemode='w')
+
+logger = logging.getLogger()
+logger.setLevel(logging.INFO)
